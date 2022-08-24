e4e2d5789bb49f262c13a47fd005ca8d27af53c4
Tyler M <t_mckenzie@outlook.com>
2022-08-24 10:24:17 -0500

    Add project files.

---
+++ ASPNetCoreWeek1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetCoreWeek1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
